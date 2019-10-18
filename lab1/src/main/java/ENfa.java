678a673211edb023caa0f37264caa778813f80e8
Matija <matijabacic98@gmail.com>
2019-10-18 13:27:27 +0200

    Bug fixes and javadoc

--- lab1/src/main/java/ENfa.java
+++ lab1/src/main/java/ENfa.java
@@ -30,6 +30,11 @@ public class ENfa {
 	// Mapping : stateFrom -> (character -> nextStates)
 	private Map<String, Map<Character, Set<String>>> transitions;
 	
+	/**
+	 * Creates ENfa with given name.
+	 * @param name name of the ENfa
+	 * @throws NullPointerException if name is <code>null</code>
+	 */
 	public ENfa(String name) {
 		this.name = Objects.requireNonNull(name);
 		
@@ -39,16 +44,34 @@ public class ENfa {
 		transitions = new HashMap<>();
 	}
 	
+	/**
+	 * Crates ENfa with given name and given starting state.
+	 * @param name name of the ENfa
+	 * @param startingState ENfa's starting state
+	 * @param startingStateAcceptability ENfa's starting state acceptability
+	 * @throws NullPointerException if name or startingState are <code>null</code>
+	 */
 	public ENfa(String name, String startingState, boolean startingStateAcceptability) {
 		this(name);
 		addState(startingState, startingStateAcceptability);
 		setStartingState(startingState);
 	}
 	
+	/**
+	 * Returns the name of the ENfa
+	 * @return the name of the ENfa
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Sets the starting state of the ENfa. Given state name must be in
+	 * ENfa's collection of states.
+	 * @param name name of the starting state
+	 * @throws NullPointerException if name is <code>null</code>
+	 * @throws IllegalArgumentException if given state name is not a state in this ENfa
+	 */
 	public void setStartingState(String name) {
 		if(!allStates.contains(Objects.requireNonNull(name))) 
 			throw new IllegalArgumentException("State with name " + name + " does not exists!");
@@ -57,6 +80,10 @@ public class ENfa {
 		reset();
 	}
 	
+	/**
+	 * Resets the ENfa (clears current state set)
+	 * @throws NullPointerException if starting state is not defined!
+	 */
 	public void reset() {
 		if(startingState==null) 
 			throw new NullPointerException("Cannot reset the ENfa : Starting state is undefined!");
@@ -65,31 +92,69 @@ public class ENfa {
 		currentActiveStates.add(startingState);
 	}
 	
+	/**
+	 * Adds a new state to the ENfa's collection of states
+	 * @param name name of the new state
+	 * @param acceptable acceptability of the new state
+	 * @throws NullPointerException if given name is <code>null</code>
+	 * @throws IllegalArgumentException if a state with the same name already exists in this ENfa
+	 */
 	public void addState(String name, boolean acceptable) {
-		if(allStates.contains(name)) throw new IllegalArgumentException("State with name " + name + " already exists!");
+		if(allStates.contains(Objects.requireNonNull(name))) throw new IllegalArgumentException("State with name " + name + " already exists!");
 		
 		allStates.add(name);
 		if(acceptable) acceptableStates.add(name);
 	}
 	
+	/**
+	 * Removes a state with the given name if one existed and resets ENfa. Does nothing otherwise.
+	 * @param name name of a state to be removed
+	 * @throws NullPointerException if given name is <code>null</code>
+	 */
 	public void removeState(String name) {
-		allStates.remove(name);
+		if(!allStates.remove(Objects.requireNonNull(name)))
+			return;
 		acceptableStates.remove(name);
 		currentActiveStates.clear();
 		transitions.remove(name);
+		
+		for(Map<Character, Set<String>> transitionMapping : transitions.values()) {
+			for(Set<String> nextStates : transitionMapping.values()) {
+				nextStates.remove(name);
+			}
+		}
+		
 		if(startingState.equals(name)) startingState = null;
+		else currentActiveStates.add(startingState);
 	}
 	
-	public void setStateAcceptability(String stateName, boolean acceptable) {
-		if(allStates.contains(stateName)) {
+	/**
+	 * If state with a given stateName exists, changes acceptability to acceptable
+	 * parameter. Otherwise, creates a new state with the given name and acceptability.
+	 * @param stateName name of the state whose acceptability is to be changed
+	 * @param acceptable acceptability of the state to be set
+	 * @return true if ENfa already contained the state, false otherwise
+	 * @throws NullPointerException if stateName is <code>null</code>
+	 */
+	public boolean setStateAcceptability(String stateName, boolean acceptable) {
+		if(allStates.contains(Objects.requireNonNull(stateName))) {
 			if(acceptable) acceptableStates.add(stateName);
 			else acceptableStates.remove(stateName);
-			return;
+			return true;
 		} 
 
 		addState(stateName, acceptable);
+		return false;
 	}
 	
+	/**
+	 * Adds a transition from 'stateFrom' to 'stateTo' with trigger 'trigger'.
+	 * @param stateFrom
+	 * @param trigger
+	 * @param stateTo
+	 * @throws NullPointerException if stateFrom or stateTo are <code>null</code>
+	 * @throws IllegalArgumentException if stateFrom or stateTo are not in ENfa
+	 */
 	public void addTransition(String stateFrom, char trigger, String stateTo) {
 		if(!allStates.contains(Objects.requireNonNull(stateFrom)) || !allStates.contains(Objects.requireNonNull(stateTo)))
 			throw new IllegalArgumentException("state1 or state2 not in enfa");
@@ -107,6 +172,14 @@ public class ENfa {
 		nextStates.add(stateTo);
 	}
 	
+	/**
+	 * Removes a transition from 'stateFrom' to 'stateTo' by 'trigger'
+	 * @param stateFrom
+	 * @param trigger
+	 * @param stateTo
+	 * @throws NullPointerException if stateFrom or stateTo are <code>null</code>
+	 * @throws IllegalArgumentException if stateFrom or stateTo are not in ENfa
+	 */
 	public void removeTransition(String stateFrom, char trigger, String stateTo) {
 		if(!allStates.contains(Objects.requireNonNull(stateFrom)) || !allStates.contains(Objects.requireNonNull(stateTo)))
 			throw new IllegalArgumentException("state1 or state2 not in enfa");
@@ -120,22 +193,45 @@ public class ENfa {
 		nextStates.remove(stateTo);
 	}
 	
+	/**
+	 * Returns an unmodifiable set of all states
+	 * @return an unmodifiable set of all states
+	 */
 	public Set<String> getAllStates() {
 		return Collections.unmodifiableSet(allStates);
 	}
 	
+	/**
+	 * Returns an unmodifiable set of all acceptable states
+	 * @return an unmodifiable set of all acceptable states
+	 */
 	public Set<String> getAcceptableStates() {
 		return Collections.unmodifiableSet(acceptableStates);
 	}
 	
+	/**
+	 * Returns starting state
+	 * @return starting state
+	 */
 	public String getStartingState() {
 		return startingState;
 	}
 	
+	/**
+	 * Returns an unmodifiable set of all current states
+	 * @return an unmodifiable set of all current states
+	 */
 	public Set<String> getCurrentActiveStates() {
 		return Collections.unmodifiableSet(currentActiveStates);
 	}
 	
+	/**
+	 * Returns an unmodifiable set of destination states for a transition
+	 * defined by 'stateFrom' and 'trigger'
+	 * @param stateFrom
+	 * @param trigger
+	 * @return an unmodifiable set of destination states
+	 */
 	public Set<String> getTransition(String stateFrom, char trigger) {
 		Map<Character, Set<String>> transitionMapping = transitions.get(stateFrom);
 		if(transitionMapping==null) return Collections.emptySet();
@@ -146,10 +242,22 @@ public class ENfa {
 		return Collections.unmodifiableSet(nextStates);
 	}
 	
+	/**
+	 * True if transition from 'stateFrom' with given 'trigger' exists, false otherwise
+	 * @param stateFrom
+	 * @param trigger
+	 * @return true if transition from 'stateFrom' with given 'trigger' exists, false otherwise
+	 */
 	public boolean transitionExists(String stateFrom, char trigger) {
 		return getTransition(stateFrom, trigger).size()!=0;
 	}
 	
+	/**
+	 * Copies all states with their acceptability and all transitions from ENfa other to
+	 * this ENfa.
+	 * @param other ENfa from which to copy
+	 * @throws IllegalAccessException if any state from other already exists in this ENfa
+	 */
 	public void copyFrom(ENfa other) {
 		for(String otherState : other.allStates) {
 			if(this.allStates.contains(otherState))
@@ -163,8 +271,23 @@ public class ENfa {
 		}
 	}
 	
+	/**
+	 * Triggers ENfa with the given trigger
+	 * @param trigger trigger with which to trigger ENfa
+	 */
 	public void step(char trigger) {
 		
+	}
+	
+	/**
+	 * Returns true if ENfa is in acceptable state (if any of current states are acceptable)
+	 * @return true if ENfa is in acceptable state
+	 */
+	public boolean isInAcceptableState() {
+		for(String state : currentActiveStates) {
+			if(acceptableStates.contains(state)) return true;
+		}
+		return false;
 	}
 	
 	@Override
