fdb38eebb197a163298f6f9fff8e56bd6b3dd07d
mlazaric <marko.lazaric@gmail.com>
2019-10-18 23:30:23 +0200

    Change goBack to Integer since it is nullable

--- lab1/src/main/java/analizator/Action.java
+++ lab1/src/main/java/analizator/Action.java
@@ -8,7 +8,7 @@ package analizator;
 public class Action {
 	
 	public int ordinalNumber;
-	public int goBack;
+	public Integer goBack;
 	public String tokenType;
 	public boolean newLine;
 	public String enterState;
