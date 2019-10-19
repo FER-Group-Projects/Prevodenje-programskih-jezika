21dcc3cd4baf5f2742a0dd8b4f0328a99c33908d
Matija <matijabacic98@gmail.com>
2019-10-19 19:37:34 +0200

    add support for serialization of LADescriptor

--- lab1/src/main/java/analizator/LADescriptor.java
+++ lab1/src/main/java/analizator/LADescriptor.java
@@ -1,5 +1,6 @@
 package analizator;
 
+import java.io.Serializable;
 import java.util.Map;
 
 /**
@@ -7,8 +8,13 @@ import java.util.Map;
  * @author Matija
  *
  */
-public class LADescriptor {
+public class LADescriptor implements Serializable {
 	
+	/**
+	 * Serial version for compatibility check between sender and receiver 
+	 */
+	private static final long serialVersionUID = -1261702926857192082L;
+
 	/**
 	 * Starting state of the LA
 	 */
