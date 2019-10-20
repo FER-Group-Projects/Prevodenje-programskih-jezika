855e82fe05d570b9549e04e3a1fca398fd671e30
mlazaric <marko.lazaric@gmail.com>
2019-10-20 22:02:07 +0200

    Change format of test examples

--- lab1/evaluate.sh
+++ lab1/evaluate.sh
@@ -10,13 +10,13 @@ do
     echo "Evaluating $line"
     echo " - generating language definition"
 
-    java -classpath $CLASSPATH GLA < $TEST_EXAMPLES/$line/language_definition || exit 1
+    java -classpath $CLASSPATH GLA < $TEST_EXAMPLES/$line/test.lan || exit 1
 
     echo " - running program through lexical analysis"
 
-    java -classpath $CLASSPATH analizator.LA < $TEST_EXAMPLES/$line/program > $TEST_EXAMPLES/$line/actual_output || exit 1
+    java -classpath $CLASSPATH analizator.LA < $TEST_EXAMPLES/$line/test.in > $TEST_EXAMPLES/$line/actual_output || exit 1
 
     echo " - comparing results"
 
-    diff --side-by-side --ignore-blank-lines $TEST_EXAMPLES/$line/actual_output $TEST_EXAMPLES/$line/expected_output || exit 1
+    diff --side-by-side --ignore-blank-lines $TEST_EXAMPLES/$line/actual_output $TEST_EXAMPLES/$line/test.out || exit 1
 done
