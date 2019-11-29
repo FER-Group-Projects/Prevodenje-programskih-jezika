95b8dc7c717a44572cb48e223a6576d65654c23a
Tin Blazevic <tin.blazevic6@gmail.com>
2019-11-29 11:48:03 +0100

    Serialization

--- lab2/src/main/java/analizator/DescriptorSerializer.java
+++ lab2/src/main/java/analizator/DescriptorSerializer.java
@@ -1,15 +1,34 @@
 package analizator;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class DescriptorSerializer {
     public static final String GENERATOR_PATH_TO_SA_DESCRIPTOR = "analizator/sa-descriptor";
     public static final String ANALYZER_PATH_TO_SA_DESCRIPTOR = "analizator/sa-descriptor";
 
-    public static void serialize(SADescriptor descriptor){
-        //TODO
+    public static void serialize(SADescriptor descriptor)throws IOException{
+        FileOutputStream fileOutputStream = new FileOutputStream(GENERATOR_PATH_TO_SA_DESCRIPTOR);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(descriptor);
+        objectOutputStream.flush();
+        objectOutputStream.close();
     }
 
     public static SADescriptor deserialize(){
-        //TODO
-        return null;
+        Path path = Paths.get(ANALYZER_PATH_TO_SA_DESCRIPTOR);
+
+        if (Files.notExists(path)) {
+            throw new IllegalArgumentException("File " + ANALYZER_PATH_TO_SA_DESCRIPTOR + " does not exist.");
+        }
+
+        try (FileInputStream fis = new FileInputStream(ANALYZER_PATH_TO_SA_DESCRIPTOR);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            return (SADescriptor) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
