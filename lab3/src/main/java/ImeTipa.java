eb2da76d629d35ae5acd79de811dff5fa67b1ce5
Tin Blazevic <tin.blazevic6@gmail.com>
2019-12-22 20:35:58 +0100

    Implement ImeTipa

--- lab3/src/main/java/ImeTipa.java
+++ lab3/src/main/java/ImeTipa.java
@@ -3,7 +3,29 @@ public class ImeTipa extends Node {
     @Override
     public Node analyze() {
         if (rightSideType == -1) determineRightSideType();
-        //TODO
+        if (currentRightSideIndex >= rightSide.size()) return null;
+        switch (rightSideType) {
+            case 0:
+                if (currentRightSideIndex == 0) {
+                    currentRightSideIndex++;
+                    return rightSide.get(currentRightSideIndex - 1);
+                }
+                break;
+            case 1:
+                if (currentRightSideIndex == 0) {
+                    currentRightSideIndex++;
+                    return rightSide.get(currentRightSideIndex);
+                } else {
+                    if (rightSide.get(1).properties.getTip() == Type.VOID) {
+                        errorHappened();
+                    } else if (rightSide.get(1).properties.getTip() == Type.INT) {
+                        properties.setTip(Type.CONST_INT);
+                    } else if (rightSide.get(1).properties.getTip() == Type.CHAR) {
+                        properties.setTip(Type.CONST_CHAR);
+                    }
+                }
+                break;
+        }
         return null;
     }
 
@@ -14,7 +36,12 @@ public class ImeTipa extends Node {
 
     @Override
     public void determineRightSideType() {
-        //TODO
+        int len = rightSide.size();
+        if (len == 1) {
+            rightSideType = 0;
+        } else {
+            rightSideType = 1;
+        }
     }
 }
 
