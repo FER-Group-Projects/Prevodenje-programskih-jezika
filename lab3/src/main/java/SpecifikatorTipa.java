493cd68076ede5cbc31209b3bc54c6c67e15154e
Tin Blazevic <tin.blazevic6@gmail.com>
2019-12-22 20:02:17 +0100

    Implement SpecifikatorTipa.java

--- lab3/src/main/java/SpecifikatorTipa.java
+++ lab3/src/main/java/SpecifikatorTipa.java
@@ -3,7 +3,7 @@ public class SpecifikatorTipa extends Node {
     @Override
     public Node analyze() {
         if (rightSideType == -1) determineRightSideType();
-        //TODO
+        //property analysis done in determineRightSideType()
         return null;
     }
 
@@ -14,7 +14,21 @@ public class SpecifikatorTipa extends Node {
 
     @Override
     public void determineRightSideType() {
-        //TODO
+        String idn = ((UniformCharacter) rightSide.get(0)).getIdentifier();
+        switch (idn) {
+            case Identifiers.KR_VOID:
+                rightSideType = 0;
+                properties.setTip(Type.VOID);
+                break;
+            case Identifiers.KR_CHAR:
+                rightSideType = 1;
+                properties.setTip(Type.CHAR);
+                break;
+            case Identifiers.KR_INT:
+                rightSideType = 2;
+                properties.setTip(Type.INT);
+                break;
+        }
     }
 }
 
