bde11e46fed9018a990063952f689cdee3f34fb3
mlazaric <marko.lazaric@gmail.com>
2019-12-23 21:58:37 +0100

    Implement <slozena naredba>
    Closes #65

--- lab3/src/main/java/Node.java
+++ lab3/src/main/java/Node.java
@@ -63,4 +63,11 @@ public abstract class Node {
         }
     }
 
+    public void setBlockTable(BlockTable blockTable) {
+        this.blockTable = blockTable;
+    }
+
+    public BlockTable getBlockTable() {
+        return blockTable;
+    }
 }
