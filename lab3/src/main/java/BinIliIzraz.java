d191b12c850715258bbc422ae77d15af1279b992
mlazaric <marko.lazaric@gmail.com>
2019-12-23 12:58:43 +0100

    Implement bin_*_izraz

--- lab3/src/main/java/BinIliIzraz.java
+++ lab3/src/main/java/BinIliIzraz.java
@@ -3,7 +3,52 @@ public class BinIliIzraz extends Node {
     @Override
     public Node analyze() {
         if (rightSideType == -1) determineRightSideType();
-        //TODO
+
+        switch (rightSideType) {
+            case 0:
+                if (currentRightSideIndex == 0) {
+                    currentRightSideIndex++;
+
+                    return rightSide.get(0);
+                }
+                else {
+                    properties.setTip(rightSide.get(0).properties.getTip());
+                    properties.setlIzraz(rightSide.get(0).properties.getlIzraz());
+
+                    return null;
+                }
+
+            case 1:
+                if (currentRightSideIndex == 0) {
+                    currentRightSideIndex++;
+
+                    return rightSide.get(currentRightSideIndex - 1);
+                }
+                else if (currentRightSideIndex == 1) {
+                    // Skip uniform character
+                    currentRightSideIndex += 2;
+
+                    if (!Checkers.checkImplicitCast(Type.INT, rightSide.get(0).properties.getTip())) {
+                        errorHappened();
+                    }
+
+                    return rightSide.get(currentRightSideIndex - 1);
+                }
+                else {
+                    if (!Checkers.checkImplicitCast(Type.INT, rightSide.get(2).properties.getTip())) {
+                        errorHappened();
+                    }
+
+                    properties.setTip(Type.INT);
+                    properties.setlIzraz(0);
+
+                    return null;
+                }
+
+            default:
+                errorHappened();
+        }
+
         return null;
     }
 
@@ -14,7 +59,16 @@ public class BinIliIzraz extends Node {
 
     @Override
     public void determineRightSideType() {
-        //TODO
+        switch (rightSide.get(0).getName()) {
+            case LeftSideNames.BIN_XILI_IZRAZ:
+                rightSideType = 0;
+                break;
+            case LeftSideNames.BIN_ILI_IZRAZ:
+                rightSideType = 1;
+                break;
+            default:
+                errorHappened();
+        }
     }
 }
 
