00a68e7147a366e1e8d916be76c13989aa0f849b
Tin Blazevic <tin.blazevic6@gmail.com>
2019-12-24 19:14:06 +0100

    Fix Inicijalizator

--- lab3/src/main/java/Inicijalizator.java
+++ lab3/src/main/java/Inicijalizator.java
@@ -7,25 +7,39 @@ public class Inicijalizator extends Node {
     public Node analyze() {
         if (rightSideType == -1) determineRightSideType();
 
-        if (currentRightSideIndex == 0) {
-            currentRightSideIndex++;
-            return rightSide.get(0);
-        } else if (currentRightSideIndex == 1) {
-            currentRightSideIndex++;
-            Type izrazTip = rightSide.get(0).properties.getTip();
-            if (izrazTip == Type.CONST_ARRAY_CHAR) {
-                int brelem = rightSide.get(0).properties.getBrElem();
-                List<Type> tipovi = new ArrayList<>();
-                for (int i = 0; i < brelem; i++) {
-                    tipovi.add(Type.CHAR);
+        switch (rightSideType) {
+            case 0:
+                if (currentRightSideIndex == 0) {
+                    currentRightSideIndex++;
+                    return rightSide.get(0);
+                } else if (currentRightSideIndex == 1) {
+                    currentRightSideIndex++;
+                    Type izrazTip = rightSide.get(0).properties.getTip();
+                    if (izrazTip == Type.CONST_ARRAY_CHAR) {
+                        int brelem = rightSide.get(0).properties.getBrElem();
+                        List<Type> tipovi = new ArrayList<>();
+                        for (int i = 0; i < brelem; i++) {
+                            tipovi.add(Type.CHAR);
+                        }
+                        properties.setBrElem(brelem);
+                        properties.setTipovi(tipovi);
+                    } else {
+                        properties.setTip(izrazTip);
+                    }
                 }
-                properties.setBrElem(brelem);
-                properties.setTipovi(tipovi);
-            } else {
-                properties.setTip(izrazTip);
-            }
+                break;
+            case 1:
+                if (currentRightSideIndex == 0) {
+                    currentRightSideIndex++;
+                    return rightSide.get(1);
+                } else if (currentRightSideIndex == 1) {
+                    currentRightSideIndex++;
+                    Node lista = rightSide.get(1);
+                    properties.setBrElem(lista.properties.getBrElem());
+                    properties.setTipovi(lista.properties.getTipovi());
+                }
+                break;
         }
-
         return null;
     }
 
@@ -39,6 +53,8 @@ public class Inicijalizator extends Node {
         int len = rightSide.size();
         if (len == 1) {
             rightSideType = 0;
+        } else if (len == 3) {
+            rightSideType = 1;
         } else {
             errorHappened();
         }
