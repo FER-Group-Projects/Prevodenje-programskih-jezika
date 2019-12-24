ca8d7c99e40eeb0d935b31bc35598bc3ccad3cbd
Matija <matijabacic98@gmail.com>
2019-12-24 01:25:25 +0100

    Implemented <naredba_grananja> and <prijevodna_jedinica>

--- lab3/src/main/java/NaredbaGrananja.java
+++ lab3/src/main/java/NaredbaGrananja.java
@@ -1,9 +1,47 @@
 public class NaredbaGrananja extends Node {
 
-    @Override
+	 @Override
     public Node analyze() {
         if (rightSideType == -1) determineRightSideType();
-        //TODO
+        
+        switch (rightSideType) {
+	        case 0:
+	        	if (currentRightSideIndex == 0) {
+	                currentRightSideIndex+=3;
+	                return rightSide.get(2);
+	            } else if (currentRightSideIndex == 3) {
+	            	if (!Checkers.checkImplicitCast(rightSide.get(2).properties.getTip(), Type.INT)) {
+                        errorHappened();
+                    }
+	            	
+	                currentRightSideIndex+=2;
+	                return rightSide.get(4);
+	            } else {
+	                return null;
+	            }
+	
+	        case 1:
+	        	if (currentRightSideIndex == 0) {
+	                currentRightSideIndex+=3;
+	                return rightSide.get(2);
+	            } else if (currentRightSideIndex == 3) {
+	            	if (!Checkers.checkImplicitCast(rightSide.get(2).properties.getTip(), Type.INT)) {
+                        errorHappened();
+                    }
+	            	
+	                currentRightSideIndex+=2;
+	                return rightSide.get(4);
+	            } else if (currentRightSideIndex == 5) {	            	
+	                currentRightSideIndex+=2;
+	                return rightSide.get(6);
+	            } else {
+	                return null;
+	            }
+	
+	        default:
+	            errorHappened();
+        }
+        
         return null;
     }
 
@@ -14,7 +52,17 @@ public class NaredbaGrananja extends Node {
 
     @Override
     public void determineRightSideType() {
-        //TODO
+    	switch (rightSide.size()) {
+	        case 5:
+	            rightSideType = 0;
+	            break;
+	        case 7:
+	            rightSideType = 1;
+	            break;
+	        default:
+	            errorHappened();
+    	}
     }
+    
 }
 
--- lab3/src/main/java/PrijevodnaJedinica.java
+++ lab3/src/main/java/PrijevodnaJedinica.java
@@ -3,7 +3,31 @@ public class PrijevodnaJedinica extends Node {
     @Override
     public Node analyze() {
         if (rightSideType == -1) determineRightSideType();
-        //TODO
+        
+        switch (rightSideType) {
+	        case 0:
+	            if (currentRightSideIndex == 0) {
+	                currentRightSideIndex++;
+	                return rightSide.get(0);
+	            } else {
+	                return null;
+	            }
+	
+	        case 1:
+	            if (currentRightSideIndex == 0) {
+	                currentRightSideIndex++;
+	                return rightSide.get(0);
+	            } else if (currentRightSideIndex == 1) {
+	                currentRightSideIndex++;
+	                return rightSide.get(1);
+	            } else {
+	                return null;
+	            }
+	
+	        default:
+	            errorHappened();
+        }
+        
         return null;
     }
 
@@ -14,7 +38,16 @@ public class PrijevodnaJedinica extends Node {
 
     @Override
     public void determineRightSideType() {
-        //TODO
+    	switch (rightSide.get(0).getName()) {
+	        case LeftSideNames.VANJSKA_DEKLARACIJA:
+	            rightSideType = 0;
+	            break;
+	        case LeftSideNames.PRIJEVODNA_JEDINICA:
+	            rightSideType = 1;
+	            break;
+	        default:
+	            errorHappened();
+    	}
     }
 }
 
